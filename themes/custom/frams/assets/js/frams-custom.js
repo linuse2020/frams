3614f360eee8e41d05023498b67ee7deaa21233f
Nitheesh Chandrika <nitheesh@origineight.net>
2020-12-30 15:49:04 +0530

    Initial site setup

---
+++ themes/custom/frams/assets/js/frams-custom.js
@@ -0,0 +1,14 @@
+/*
+ *  Custom JS
+ *
+ */
+
+(function ($) {
+  // Store our function as a property of Drupal.behaviors.
+  Drupal.behaviors.starratings = {
+    attach: function (context, settings) {
+      // code here
+    }
+  };
+
+}(jQuery));
