89b692bf05d4ef947c3416b71bf3cb618126ff75
linuse <linus.tech19@gmail.com>
2021-03-13 08:33:38 +0530

    delete module backup

--- vendor/composer/InstalledVersions.php
+++ vendor/composer/InstalledVersions.php
@@ -19,7 +19,7 @@ private static $installed = array (
     'aliases' => 
     array (
     ),
-    'reference' => '72e1930ebbcbc44a7f41fea52d20f6adaac62701',
+    'reference' => '6ea6994f122fcc8d1fec5d59fcbdce15d08a45ca',
     'name' => 'drupal/legacy-project',
   ),
   'versions' => 
@@ -206,15 +206,6 @@ private static $installed = array (
       array (
         0 => '8.9.13',
       ),
-    ),
-    'drupal/backup_migrate' => 
-    array (
-      'pretty_version' => '4.1.0',
-      'version' => '4.1.0.0',
-      'aliases' => 
-      array (
-      ),
-      'reference' => '8.x-4.1',
     ),
     'drupal/ban' => 
     array (
@@ -1180,7 +1171,7 @@ private static $installed = array (
       'aliases' => 
       array (
       ),
-      'reference' => '72e1930ebbcbc44a7f41fea52d20f6adaac62701',
+      'reference' => '6ea6994f122fcc8d1fec5d59fcbdce15d08a45ca',
     ),
     'drupal/libraries' => 
     array (
